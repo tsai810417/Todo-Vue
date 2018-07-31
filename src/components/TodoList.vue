05a2af28a12a2b6652c9bbc248966da8ef398b01
tsai810417 <tsai_cheng@yahoo.com>
2018-07-31 17:02:56 +0800

    create a todo-list component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="">
+    <ul>
+      <li>Todo A</li>
+      <li>Todo B</li>
+      <li>Todo C</li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+}
+</script>
+
+<style lang="css">
+</style>

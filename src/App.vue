a2d0a81a3e6cfede9183020307f65bb92251fdb1
tsai810417 <tsai_cheng@yahoo.com>
2018-07-31 17:35:17 +0800

    pass todos as props to the componenet and render all the informations

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <todo-list/>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -12,7 +12,33 @@ export default {
   components: {
     TodoList,
   },
+  data() {
+    return {
+      todos: [{
+        id: 1,
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        id: 2,
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        id: 3,
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        id: 4,
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
 };
+
 </script>
 
 <style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,16 +1,28 @@
 <template lang="html">
-  <div class="">
-    <ul>
-      <li>Todo A</li>
-      <li>Todo B</li>
-      <li>Todo C</li>
-    </ul>
+  <div>
+    <div class="centered card" v-for="todo in todos" v-bind:key="todo.id">
+      <div class="content">
+        <div class="header">
+          {{ todo.title }}
+        </div>
+        <div class="meta">
+          {{ todo.project }}
+        </div>
+        <div v-show="todo.done">
+          Completed
+        </div>
+        <div v-show="!todo.done">
+          Incomplete
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/javascript">
 export default {
-}
+  props: ['todos'],
+};
 </script>
 
 <style lang="css">

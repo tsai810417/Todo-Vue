b618853c27c0dba7fddffb12985bf5fac31add39
tsai810417 <tsai_cheng@yahoo.com>
2018-08-02 11:02:32 +0800

    add title and counts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <p class="countTasks">Completed Tasks: {{ todos.filter(todo => {  return todo.done === true }).length }}</p>
+    <p class="countTasks">Incompleted Tasks: {{ todos.filter(todo => {  return todo.done === false }).length }}</p>
     <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
